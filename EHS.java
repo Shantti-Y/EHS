102aee2198dc43adea2c0e2ab444eb989372cc90
Takahiro Yoshioka <tsolvalouy@gmail.com>
2017-07-08 14:43:56 +0000

    Build overall structure and relative mockup

---
+++ EHS.java
@@ -0,0 +1,21 @@
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class EHS extends Application {
+   public static void main(String[] args) {
+      launch(args);
+   }
+
+   @Override
+   public void start(Stage stage) throws Exception {
+      BorderPane root;
+      root = (BorderPane)FXMLLoader.load(getClass().getResource("content.fxml"));
+      Scene scene = new Scene(root, 350, 320);
+      stage.setScene(scene);
+      stage.show();
+   }
+}
