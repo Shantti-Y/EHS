571d7e05c59303a8451e65419385cee489955a0b
Takahiro Yoshioka <tsolvalouy@gmail.com>
2017-09-29 06:08:47 +0000

    Create appearance on gui page as draft

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,7 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 375, height: 380})
 
   // and load the index.html of the app.
   win.loadURL(url.format({
@@ -17,8 +17,6 @@ function createWindow () {
     slashes: true
   }))
 
-  // Open the DevTools.
-  win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
