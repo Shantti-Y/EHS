815de01bbe3fb89f6a4a17e83f6525195530cdc2
Takahiro Yoshioka <tsolvalouy@gmail.com>
2017-10-09 13:33:19 +0000

    Creating rolling unit function

--- scripts/page.js
+++ scripts/page.js
@@ -1,6 +1,13 @@
 
 window.addEventListener('load', () => {
 
+   class Unit {
+      constructor(digit, element){
+         this.digit = digit
+         this.element = element
+      }
+   }
+
    // Initial properties
    let pointer = document.getElementById('pointer')
    const meter_bar = document.getElementById('meter-bar')
@@ -8,17 +15,50 @@ window.addEventListener('load', () => {
    const EDGE_RIGHT = meter_bar.getBoundingClientRect().width
    let x = 0
 
-   const movePointerToRight = () => {
-      x += 1
-      pointer.style.left = x + 'px'
+   let units = new Array()
+   let unit_elements = document.getElementsByClassName('unit')
+   const UNIT_BOTTOM = unit_elements[0].getBoundingClientRect().height + 6
+   const UNIT_TOP = -1 * (UNIT_BOTTOM + 6)
+
+   for(let i = 0; i < 5; i++){
+      // element property in Unit class gets span tag inside li.unit
+      let digit = i + 1
+      let unit_element = unit_elements[unit_elements.length - digit].children[0]
+      units[i] = new Unit(digit, unit_element)
+      console.log(units[i].element.offsetTop)
    }
 
-   setInterval(() => {
+   let y = 0
+   const moveUnit = () => {
+      for(let key in units){
+         let unit = units[key]
+         if(y % (5 ** unit.digit) == 0){
+            let unit_position = unit.element.offsetTop - 1
+            if(unit_position == UNIT_TOP){
+               unit_position = UNIT_BOTTOM
+            }
+            unit.element.style.top = unit_position + 'px'
+         }
+
+      }
+   }
+
+   const movePointer = () => {
       if(x == EDGE_RIGHT){
          x = EDGE_LEFT
       }else{
-         movePointerToRight()
+         x += 1
       }
+      y += 1
+      pointer.style.left = x + 'px'
+      moveUnit()
+   }
+
+   setInterval(() => {
+         movePointer()
    }, 5)
+
+
+   // 5桁のメーターをいじる
 
 })
--- src/page.js
+++ src/page.js
@@ -1,24 +1,65 @@
 'use strict';
 
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
 window.addEventListener('load', function () {
+   var Unit = function Unit(digit, element) {
+      _classCallCheck(this, Unit);
+
+      this.digit = digit;
+      this.element = element;
+   };
 
    // Initial properties
+
+
    var pointer = document.getElementById('pointer');
    var meter_bar = document.getElementById('meter-bar');
    var EDGE_LEFT = -10;
    var EDGE_RIGHT = meter_bar.getBoundingClientRect().width;
    var x = 0;
 
-   var movePointerToRight = function movePointerToRight() {
-      x += 1;
-      pointer.style.left = x + 'px';
+   var units = new Array();
+   var unit_elements = document.getElementsByClassName('unit');
+   var UNIT_BOTTOM = unit_elements[0].getBoundingClientRect().height + 6;
+   var UNIT_TOP = -1 * (UNIT_BOTTOM + 6);
+
+   for (var i = 0; i < 5; i++) {
+      // element property in Unit class gets span tag inside li.unit
+      var digit = i + 1;
+      var unit_element = unit_elements[unit_elements.length - digit].children[0];
+      units[i] = new Unit(digit, unit_element);
+      console.log(units[i].element.offsetTop);
+   }
+
+   var y = 0;
+   var moveUnit = function moveUnit() {
+      for (var key in units) {
+         var unit = units[key];
+         if (y % 5 ** unit.digit == 0) {
+            var unit_position = unit.element.offsetTop - 1;
+            if (unit_position == UNIT_TOP) {
+               unit_position = UNIT_BOTTOM;
+            }
+            unit.element.style.top = unit_position + 'px';
+         }
+      }
    };
 
-   setInterval(function () {
+   var movePointer = function movePointer() {
       if (x == EDGE_RIGHT) {
          x = EDGE_LEFT;
       } else {
-         movePointerToRight();
+         x += 1;
       }
+      y += 1;
+      pointer.style.left = x + 'px';
+      moveUnit();
+   };
+
+   setInterval(function () {
+      movePointer();
    }, 5);
+
+   // 5桁のメーターをいじる
 });
