0802c5b626e8398a5019eade36b31015339f1f97
Takahiro Yoshioka <tsolvalouy@gmail.com>
2017-10-10 15:15:29 +0000

    Create Classes of each tag for control

--- scripts/page.js
+++ scripts/page.js
@@ -2,46 +2,82 @@
 window.addEventListener('load', () => {
 
    class Unit {
-      constructor(digit, element, number){
-         this.digit = digit
+      constructor(element, digit, number){
          this.element = element
+         this.digit = digit
          this.number = number
       }
 
+      dimension(){
+         return {
+            width:   this.element.parentElement.getBoundingClientRect().width,
+            height:  this.element.parentElement.getBoundingClientRect().height,
+            top:     (-1 * (this.element.parentElement.getBoundingClientRect().height)) + 18,
+            bottom:  5
+         }
+      }
+
       insertNumber (){
+         let next_number = this.number + 1
          if(this.number == 10){
             this.number = 0
+            next_number = 1
+         }else if(this.number == 9){
+            next_number = 0
          }
-         if(this.number == 9){
-            this.element.innerHTML = this.number + '<br>' + 0
-         }else{
-            this.element.innerHTML = this.number + '<br>' + (this.number + 1)
+         this.element.innerHTML = this.number + '<br>' + next_number
+      }
+   }
+
+   class Pointer {
+      constructor(element, position){
+         this.element = element
+         this.position = position
+      }
+
+      dimension(){
+         return {
+            width:   this.element.getBoundingClientRect().height,
+            height:  this.element.getBoundingClientRect().width
+         }
+      }
+
+      moveToRight(start_point, end_point){
+         this.position = returnToBeginning(this.position, end_point, start_point, 'plus')
+         this.element.style.left = this.position + 'px'
+      }
+   }
+
+   class MeterBar {
+      constructor(element){
+         this.element = element
+      }
+
+      dimension(){
+         return {
+            width:   this.element.getBoundingClientRect().width + 10,
+            left:    -10,
+            right:   this.element.getBoundingClientRect().width
          }
       }
    }
 
    // Initial properties
-   let pointer = document.getElementById('pointer')
-   const meter_bar = document.getElementById('meter-bar')
-   const EDGE_LEFT = -10
-   const EDGE_RIGHT = meter_bar.getBoundingClientRect().width
-   let x = 0
+   let pointer = new Pointer(document.getElementById('pointer'), 0)
+   const meter_bar = new MeterBar(document.getElementById('meter-bar'))
 
+   // Initializing units
    let units = new Array()
    let unit_elements = document.getElementsByClassName('unit')
-
-   const UNIT_BOTTOM = 5
-   const UNIT_TOP = (-1 * unit_elements[0].getBoundingClientRect().height) + 18
 
    for(let i = 0; i < 5; i++){
       // element property in Unit class gets span tag inside li.unit
       let digit = i + 1
       let unit_element = unit_elements[unit_elements.length - digit].children[0]
-      units[i] = new Unit(digit, unit_element, 0)
+      units[i] = new Unit(unit_element, digit, 0)
    }
 
-   let y = 0
-
+   // going to be in module file
    const returnToBeginning = (position, limit, beginning, unit) => {
       if(position == limit){
          position = beginning
@@ -51,15 +87,17 @@ window.addEventListener('load', () => {
       return position
    }
 
-   const UNIT_HEIGHT = unit_elements[0].getBoundingClientRect().height
-   const METER_WIDTH = meter_bar.getBoundingClientRect().width + 10
-
    const isApproachedTo10 = (key) => {
       let unit = units[key]
-      let calc = Math.floor(METER_WIDTH / UNIT_HEIGHT) * (10 ** unit.digit)
+
+      // While the pointer tag goes to the edge of its movement range,
+      // each unit goes up to the beyond its flame.
+      // Their speed are calculated by the following formula.
+      let calc = Math.floor(meter_bar.dimension().width / unit.dimension().height) * (10 ** unit.digit)
+
       if(y % calc == 0){
-         let unit_position = returnToBeginning(unit.element.offsetTop, UNIT_TOP, UNIT_BOTTOM, 'minus')
-         if(unit_position == UNIT_BOTTOM){
+         let unit_position = returnToBeginning(unit.element.offsetTop, unit.dimension().top, unit.dimension().bottom, 'minus')
+         if(unit_position == unit.dimension().bottom){
             unit.number += 1
             unit.insertNumber()
          }
@@ -72,18 +110,12 @@ window.addEventListener('load', () => {
       }
    }
 
-   const moveUnit = () => {
-      isApproachedTo10(0)
-   }
-
-   const movePointer = () => {
-      y += 1
-      x = returnToBeginning(x, EDGE_RIGHT, EDGE_LEFT, 'plus')
-      pointer.style.left = x + 'px'
-      moveUnit()
-   }
+   // main function
+   let pointer_distance = 0
 
    let intervalController = setInterval(() => {
-      movePointer()
-   }, 0.25)
+      pointer_distance += 1
+      pointer.moveToRight(meter_bar.dimension().left, meter_bar.dimension().right)
+      isApproachedTo10(0)
+   }, 0.001)
 })
--- src/scripts/page.js
+++ src/scripts/page.js
@@ -6,55 +6,107 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
 window.addEventListener('load', function () {
    var Unit = function () {
-      function Unit(digit, element, number) {
+      function Unit(element, digit, number) {
          _classCallCheck(this, Unit);
 
-         this.digit = digit;
          this.element = element;
+         this.digit = digit;
          this.number = number;
       }
 
       _createClass(Unit, [{
+         key: 'dimension',
+         value: function dimension() {
+            return {
+               width: this.element.parentElement.getBoundingClientRect().width,
+               height: this.element.parentElement.getBoundingClientRect().height,
+               top: -1 * this.element.parentElement.getBoundingClientRect().height + 18,
+               bottom: 5
+            };
+         }
+      }, {
          key: 'insertNumber',
          value: function insertNumber() {
+            var next_number = this.number + 1;
             if (this.number == 10) {
                this.number = 0;
+               next_number = 1;
+            } else if (this.number == 9) {
+               next_number = 0;
             }
-            if (this.number == 9) {
-               this.element.innerHTML = this.number + '<br>' + 0;
-            } else {
-               this.element.innerHTML = this.number + '<br>' + (this.number + 1);
-            }
+            this.element.innerHTML = this.number + '<br>' + next_number;
          }
       }]);
 
       return Unit;
    }();
 
+   var Pointer = function () {
+      function Pointer(element, position) {
+         _classCallCheck(this, Pointer);
+
+         this.element = element;
+         this.position = position;
+      }
+
+      _createClass(Pointer, [{
+         key: 'dimension',
+         value: function dimension() {
+            return {
+               width: this.element.getBoundingClientRect().height,
+               height: this.element.getBoundingClientRect().width
+            };
+         }
+      }, {
+         key: 'moveToRight',
+         value: function moveToRight(start_point, end_point) {
+            this.position = returnToBeginning(this.position, end_point, start_point, 'plus');
+            this.element.style.left = this.position + 'px';
+         }
+      }]);
+
+      return Pointer;
+   }();
+
+   var MeterBar = function () {
+      function MeterBar(element) {
+         _classCallCheck(this, MeterBar);
+
+         this.element = element;
+      }
+
+      _createClass(MeterBar, [{
+         key: 'dimension',
+         value: function dimension() {
+            return {
+               width: this.element.getBoundingClientRect().width + 10,
+               left: -10,
+               right: this.element.getBoundingClientRect().width
+            };
+         }
+      }]);
+
+      return MeterBar;
+   }();
+
    // Initial properties
 
 
-   var pointer = document.getElementById('pointer');
-   var meter_bar = document.getElementById('meter-bar');
-   var EDGE_LEFT = -10;
-   var EDGE_RIGHT = meter_bar.getBoundingClientRect().width;
-   var x = 0;
+   var pointer = new Pointer(document.getElementById('pointer'), 0);
+   var meter_bar = new MeterBar(document.getElementById('meter-bar'));
 
+   // Initializing units
    var units = new Array();
    var unit_elements = document.getElementsByClassName('unit');
-
-   var UNIT_BOTTOM = 5;
-   var UNIT_TOP = -1 * unit_elements[0].getBoundingClientRect().height + 18;
 
    for (var i = 0; i < 5; i++) {
       // element property in Unit class gets span tag inside li.unit
       var digit = i + 1;
       var unit_element = unit_elements[unit_elements.length - digit].children[0];
-      units[i] = new Unit(digit, unit_element, 0);
+      units[i] = new Unit(unit_element, digit, 0);
    }
 
-   var y = 0;
-
+   // going to be in module file
    var returnToBeginning = function returnToBeginning(position, limit, beginning, unit) {
       if (position == limit) {
          position = beginning;
@@ -64,15 +116,17 @@ window.addEventListener('load', function () {
       return position;
    };
 
-   var UNIT_HEIGHT = unit_elements[0].getBoundingClientRect().height;
-   var METER_WIDTH = meter_bar.getBoundingClientRect().width + 10;
-
    var isApproachedTo10 = function isApproachedTo10(key) {
       var unit = units[key];
-      var calc = Math.floor(METER_WIDTH / UNIT_HEIGHT) * 10 ** unit.digit;
+
+      // While the pointer tag goes to the edge of its movement range,
+      // each unit goes up to the beyond its flame.
+      // Their speed are calculated by the following formula.
+      var calc = Math.floor(meter_bar.dimension().width / unit.dimension().height) * 10 ** unit.digit;
+
       if (y % calc == 0) {
-         var unit_position = returnToBeginning(unit.element.offsetTop, UNIT_TOP, UNIT_BOTTOM, 'minus');
-         if (unit_position == UNIT_BOTTOM) {
+         var unit_position = returnToBeginning(unit.element.offsetTop, unit.dimension().top, unit.dimension().bottom, 'minus');
+         if (unit_position == unit.dimension().bottom) {
             unit.number += 1;
             unit.insertNumber();
          }
@@ -85,18 +139,12 @@ window.addEventListener('load', function () {
       }
    };
 
-   var moveUnit = function moveUnit() {
-      isApproachedTo10(0);
-   };
-
-   var movePointer = function movePointer() {
-      y += 1;
-      x = returnToBeginning(x, EDGE_RIGHT, EDGE_LEFT, 'plus');
-      pointer.style.left = x + 'px';
-      moveUnit();
-   };
+   // main function
+   var pointer_distance = 0;
 
    var intervalController = setInterval(function () {
-      movePointer();
-   }, 0.25);
+      pointer_distance += 1;
+      pointer.moveToRight(meter_bar.dimension().left, meter_bar.dimension().right);
+      isApproachedTo10(0);
+   }, 0.001);
 });
